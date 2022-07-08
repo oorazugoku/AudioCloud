54fe9498e3c9b0811af71dd97492ccecb046a5a6
Ben Durham <oorazugoku@gmail.com>
2022-07-08 03:39:40 -0500

    phase 5 done

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -1,12 +1,45 @@
 const express = require('express')
 const router = express.Router();
-
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
 const { setTokenCookie, restoreUser } = require('../../utils/auth');
 const { User } = require('../../db/models');
 
+const validateLogin = [
+  check('credential')
+    .exists({ checkFalsy: true })
+    .notEmpty()
+    .withMessage('Please provide a valid email or username.'),
+  check('password')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a password.'),
+  handleValidationErrors
+];
+
+const validateSignup = [
+  check('email')
+    .exists({ checkFalsy: true })
+    .isEmail()
+    .withMessage('Please provide a valid email.'),
+  check('username')
+    .exists({ checkFalsy: true })
+    .isLength({ min: 4 })
+    .withMessage('Please provide a username with at least 4 characters.'),
+  check('username')
+    .not()
+    .isEmail()
+    .withMessage('Username cannot be an email.'),
+  check('password')
+    .exists({ checkFalsy: true })
+    .isLength({ min: 6 })
+    .withMessage('Password must be 6 characters or more.'),
+  handleValidationErrors
+];
+
+
 
 // Login
-router.post('/login', async (req, res, next) => {
+router.post('/login', validateLogin, async (req, res, next) => {
   const { credential, password } = req.body;
   const user = await User.login({ credential, password });
   if (!user) {
@@ -31,7 +64,7 @@ router.delete('/logout', (_req, res) => {
 
 
 // Signup
-router.post('/signup', async (req, res) => {
+router.post('/signup', validateSignup, async (req, res) => {
     const { email, password, username } = req.body;
     const user = await User.signup({ email, username, password });
 
