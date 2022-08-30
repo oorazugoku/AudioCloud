f6a17eb912686b5b0088aaffa327946f0cbebacf
Ben Durham <oorazugoku@gmail.com>
2022-08-30 18:10:07 -0500

    fixed assocciations

--- backend/db/models/songlike.js
+++ backend/db/models/songlike.js
@@ -10,8 +10,8 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      Comment.belongsTo(models.Song, { foreignKey: 'songId', onDelete: 'CASCADE' });
-      Comment.belongsTo(models.User, { foreignKey: 'userId', onDelete: 'CASCADE' });
+      songLike.belongsTo(models.Song, { foreignKey: 'songId', onDelete: 'CASCADE' });
+      songLike.belongsTo(models.User, { foreignKey: 'userId', onDelete: 'CASCADE' });
     }
   }
   songLike.init({
