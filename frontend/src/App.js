5dd5ddd27a62e2784800417b56e902d7af3a454c
Ben Durham <oorazugoku@gmail.com>
2022-08-30 18:46:58 -0500

    HomePage Showing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,22 @@
 import React from "react";
+import { Route, Switch, BrowserRouter } from 'react-router-dom'
+import { ModalProvider } from './components/context/Modal';
 import HomePage from "./components/HomePage";
-import { Route, Switch } from 'react-router-dom'
 
 function App() {
   return (
-    <Switch>
-    <Route exact path="/">
-      <HomePage />
-    </Route>
-    <Route path="/">
-      Page Does Not Exist
-    </Route>
-  </Switch>
+  <ModalProvider>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path='/'>
+          <HomePage />
+        </Route>
+        <Route path="/">
+          Page Does Not Exist
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  </ModalProvider>
   );
 }
 
