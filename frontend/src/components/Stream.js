ee88feea0cdf3d292870d121174cd03667dab2dd
Ben Durham <oorazugoku@gmail.com>
2022-09-06 11:35:38 -0500

    fixed no container error for wave files

--- frontend/src/components/Stream.js
+++ frontend/src/components/Stream.js
@@ -2,50 +2,37 @@ import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import WaveSurfer from 'wavesurfer.js';
 import EditSong from "./EditSong";
-import audio from './SoBad.mp3'
+import audio1 from './SoBad.mp3'
 
 
 import './CSS/Stream.css'
 
 const Stream = ({ setLocation }) => {
     const dispatch = useDispatch();
-    const [song, setSong] = useState()
-    const songs = useSelector(state => Object.values(state.songs))
-    const users = useSelector(state => state.users)
-    const user = useSelector(state => state.session.user)
-    const [editing, setEditing] = useState(false)
+    const [song, setSong] = useState();
+    const songs = useSelector(state => Object.values(state.songs));
+    const users = useSelector(state => state.users);
+    const user = useSelector(state => state.session.user);
+    const [editing, setEditing] = useState(false);
 
 
-    const loadAudio = (url, i) => {
-        const doc = document.getElementsByClassName(`Stream-wave-${i}`)
-        const audio = WaveSurfer.create({
-                        container: doc,
-                        waveColor: 'violet',
-                        progressColor: 'purple',
-                        normalize: true,
-                        xhr: {
-                            mode: "no-cors",
-                            method: "GET",
-                            credentials: "include"
-                        }
-                    });
-                    audio.load(audio)
-    }
 
-    // useEffect(()=>{
-    //     audio = WaveSurfer.create({
-    //         container: '.Stream-left-container',
-    //         waveColor: 'violet',
-    //         progressColor: 'purple',
-    //         normalize: true,
-    //         xhr: {
-    //             mode: "no-cors",
-    //             method: "GET",
-    //             credentials: "include"
-    //         }
-    //     });
-    //     setLoaded(true)
-    // }, [])
+    useEffect(()=>{
+        songs?.map((song, i) => {
+            const audio = WaveSurfer.create({
+                container: `.Stream-wave-${i}`,
+                waveColor: 'violet',
+                progressColor: 'purple',
+                xhr: {
+                    mode: "no-cors",
+                    method: "GET",
+                    credentials: "include"
+                }
+            });
+            audio.load(`${song.url}`)
+        })
+
+    }, [])
 
     return (
         <>
@@ -58,7 +45,7 @@ const Stream = ({ setLocation }) => {
                                 <div className="Stream-song-info">
                                     <div className="Stream-song-artist">{users[song.artistId]?.username}</div>
                                     <div className="Stream-song-title">{song?.title}</div>
-                                    <div className={`Stream-wave-${i}`}>{song.url}</div>
+                                    <div className={`Stream-wave-${i}`}></div>
                                     <div className="Stream-song-description">{song?.description}</div>
                                 </div>
                                 {user.id === song.artistId && (<button className="Stream-song-edit-button" onClick={()=>{setEditing(true); setSong(song)}}>Edit</button>)}
